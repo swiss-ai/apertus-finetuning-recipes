5858877550bd508ce54781daac50f7350d9ef6e6
KaustubhP11 <ponkshekaustubh11@gmail.com>
2025-09-08 15:41:08 +0200

    first commit

---
+++ sft_train.py
@@ -0,0 +1,87 @@
+# Copyright 2020-2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+accelerate launch \
+    --config_file configs/zero3.yaml \
+    sft_train.py \
+    --config configs/sft_lora.yaml \
+    --model_name_or_path swiss-ai/Apertus-8B-Instruct-2509 \
+"""
+
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+from trl import (
+    ModelConfig,
+    ScriptArguments,
+    SFTConfig,
+    SFTTrainer,
+    TrlParser,
+    get_peft_config,
+)
+
+
+
+def main(script_args, training_args, model_args):
+    # ------------------------
+    # Load model & tokenizer
+    # ------------------------
+    #Set base directory to store model
+    store_base_dir = "./" #os.getenv("STORE")
+    
+    model_kwargs = dict(
+        torch_dtype=model_args.torch_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path, **model_kwargs
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+    )
+
+    # --------------
+    # Load dataset
+    # --------------
+    dataset = load_dataset(script_args.dataset_name, name=script_args.dataset_config)
+
+    # -------------
+    # Train model
+    # -------------
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset[script_args.dataset_train_split],
+        eval_dataset=dataset[script_args.dataset_test_split]
+        if training_args.eval_strategy != "no"
+        else None,
+        processing_class=tokenizer,
+        peft_config=get_peft_config(model_args),
+    )
+
+    trainer.train()
+    trainer.save_model(os.path.join(store_base_dir, training_args.output_dir))
+    if training_args.push_to_hub:
+        trainer.push_to_hub(dataset_name=script_args.dataset_name)
+
+
+if __name__ == "__main__":
+    parser = TrlParser((ScriptArguments, SFTConfig, ModelConfig))
+    script_args, training_args, model_args, _ = parser.parse_args_and_config(
+        return_remaining_strings=True
+    )
+    main(script_args, training_args, model_args)
